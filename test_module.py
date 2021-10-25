fa27bf7a8b706ac5bbe1cd4ee3730595c125895c
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-26 01:42:09 +0300

    Define the setup test method to be executed at the beginning of tests

--- test_module.py
+++ test_module.py
@@ -1,13 +1,20 @@
 import unittest
+from account import Account
+from credentials import Credentials
 from user import User
 
 class UserTest(unittest.TestCase):
-  """Test class that teste cases for the password locker and executions
+  """Test class that tests cases for the password locker and executions
   Args:
       unittest.TestCase
   """
   def setUp(self):
-    self.ne
+    """
+    Setup method to run before each test cases.
+    """
+    self.account_info=Account("Twitter", "Levy", "levy1010")
+    self.user_info=User("Levy", "levy1010")
+    self.credentials_info=Credentials()
 
   def tearDown(self):
     User.user_list = []
