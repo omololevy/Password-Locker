bfa9c569697e4b9cfa6e25743250a7932b843079
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-26 01:16:40 +0300

    Define a method that checks the login name and password

--- user.py
+++ user.py
@@ -15,3 +15,12 @@ class User:
   def add_new_user(cls, new_user): 
     """This adds a new user to the users list."""
     cls.user_list.append(new_user)
+
+  @classmethod
+  def check_login(cls, login_name, login_password):
+    """This method checks whether the user login details exist in the users_list"""
+    for user in cls.users_list:
+      if user.username==login_name:
+        if user.password==login_password:
+          return True
+    return False
