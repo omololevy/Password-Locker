be50f38b4d37c4421d9bdcc6345903b1753a4082
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-25 23:15:08 +0300

    Define a method that create credential and adds to the app

--- credentials.py
+++ credentials.py
@@ -1,3 +1,4 @@
+from pyperclip import init_klipper_clipboard
 import password
 import account
 
@@ -20,6 +21,39 @@ class Credentials:
     self.credntial_list.append(new_acc)
     print(f"{acc_name} account credentials added successfully!")
 
-    #if a user wants a valid generated password
+  def create_credential(self):
+    """This method create new credential and adds it to the app"""
+    print("  \n Add your credential here.")
+    print("-"*15)
+    acc_name= input("Enter account name:\n")
+    acc_userName= input("Account username:\n")
+    acc_pass= " "
+
+      #if a user wants a valid generated password
     need_password =True
+    while need_password:
+      need_syst_password=input("Do you want the password generated for you? Y/n")
+      if need_syst_password =="Y":
+        need_password= False
+        acc_pass=passcord.gen_password()
+        print(f"Your password: {acc_pass} (copied to clipboard)")
+
+      elif need_password=="n":
+        acc_pass=input("Enter Account password:\n")
+        pass_confirm=input("Confirm the password:\n")
+        if acc_pass==pass_confirm:
+          need_password=False
+
+        else:
+          print("Passwords did not match!! Try again.")
+          need_password= True
+
+      else:
+          print("..Invalid Choice!! Choose Y/n")
+          need_password=True
+
+    new_acc=account.Account(acc_name,acc_userName,acc_pass)
+    self.credntial_list.append(new_acc)
+    print(f"{acc_name} account credentials created successfully!")
+
       
