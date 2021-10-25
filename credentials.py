9cddd1adc4b77bb2c6ce1221c247a685fc3cba8c
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-25 19:19:43 +0300

    Add a method to add the existing credential to the app

--- credentials.py
+++ credentials.py
@@ -1,4 +1,25 @@
 import password
 import account
-import pyperclip
 
+passcord= password.Password()
+class Credentials:
+  """This class adds user credential."""
+  def __init__(self):
+      self.credntial_list=[]
+
+  def add_credential(self):
+    '''Adds already existing credenial to the app'''
+    print("  \n Add your credential here.")
+    print("-"*15)
+
+    acc_name= input("Enter account name:\n")
+    acc_userName= input("Account username:\n")
+    acc_pass=input("Account password:\n")
+
+    new_acc =account.Account(acc_pass, acc_userName, acc_pass)
+    self.credntial_list.append(new_acc)
+    print(f"{acc_name} account credentials added successfully!")
+
+    #if a user wants a valid generated password
+    need_password =True
+      
