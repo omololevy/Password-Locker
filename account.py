03a1da459f323cf7160ee10780fed1961dae165e
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-25 17:59:53 +0300

    Create a Account class in account module thaat generates the new instances of account credentials

---
+++ account.py
@@ -0,0 +1,8 @@
+
+class Account:
+  """This is a class to generate new instances of account credatials"""
+
+  def __init__(self, acc_name, acc_userName, acc_pass):
+    self.acc_name=acc_name
+    self.acc_userName=acc_userName
+    self.acc_pass=acc_pass
