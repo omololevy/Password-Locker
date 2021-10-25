e80ab90595b2ad82f4549ea58d41a200f1bd7c03
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-25 16:30:43 +0300

    define teardown class and userTest class. import user from user

---
+++ test_module.py
@@ -0,0 +1,13 @@
+import unittest
+from user import User
+
+class UserTest(unittest.TestCase):
+  """Test class that teste cases for the password locker and executions
+  Args:
+      unittest.TestCase
+  """
+  def setUp(self):
+    self.ne
+
+  def tearDown(self):
+    User.user_list = []
