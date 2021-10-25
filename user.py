960a650fce9dfdda616fb56f53ccb5df345df377
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-25 16:25:52 +0300

    Add the classmethod decorator that accepts the new user

--- user.py
+++ user.py
@@ -5,4 +5,7 @@ class User:
   def __init__(self, username, password):
     self.username = username
     self.password = password
-    
+
+  @classmethod
+  def add_new_user(cls, new_user): #This adds a new user to the users list.
+    cls.user_list.append(new_user)
